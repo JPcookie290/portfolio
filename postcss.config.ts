b9930684a0209c8aa3b7b65d2be0c2eaaf813702
JPcookie290 <julia.paier@yahoo.com>
2024-07-27 22:36:58 +0200

    adds postcss.config.ts, small changes tsconfig.ts ,settings.json, page.json if needed becasuse of tailwind

---
+++ postcss.config.ts
@@ -0,0 +1,8 @@
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
+const postcssConfig = {
+  plugins: [tailwindcss, autoprefixer],
+};
+
+export default postcssConfig;
