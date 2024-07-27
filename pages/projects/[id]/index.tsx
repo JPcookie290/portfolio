bb05edcd658fe410a5489252dc22b75dfcc070d4
JPcookie290 <julia.paier@yahoo.com>
2024-07-27 22:56:43 +0200

    small change in CustomHead

--- pages/projects/[id]/index.tsx
+++ pages/projects/[id]/index.tsx
@@ -3,7 +3,7 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import fs from "fs";
 import path from "path";
 import Layout from "../../../components/Layout";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import { IProject } from "../../../interfaces/Project";
 
 type ProjectPageProps = {
